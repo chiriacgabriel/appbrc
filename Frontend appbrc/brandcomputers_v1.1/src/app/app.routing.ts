d664e8813e28e792b946cd652591d30460f3871d
Gabriel Chiriac <shantajj@gmail.com>
2021-12-11 08:55:00 +0200

    Deleted un-necessary code

--- Frontend appbrc/brandcomputers_v1.1/src/app/app.routing.ts
+++ Frontend appbrc/brandcomputers_v1.1/src/app/app.routing.ts
@@ -18,30 +18,9 @@ export const AppRoutes: Routes = [{
     }, {
         path: 'components',
         loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule)
-    }, {
-        path: 'forms',
-        loadChildren: () => import('./forms/forms.module').then(m => m.Forms)
-    }, {
-        path: 'tables',
-        loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule)
-    }, {
-        path: 'maps',
-        loadChildren: () => import('./maps/maps.module').then(m => m.MapsModule)
-    }, {
-        path: 'charts',
-        loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule)
-    }, {
-        path: 'calendar',
-        loadChildren: () => import('./calendar/calendar.module').then(m => m.CalendarModule)
-    }, {
-        path: '',
-        loadChildren: () => import('./userpage/user.module').then(m => m.UserModule)
-    }, {
+    },  {
         path: '',
         loadChildren: () => import('./timeline/timeline.module').then(m => m.TimelineModule)
-    }, {
-        path: '',
-        loadChildren: () => import('./widgets/widgets.module').then(m => m.WidgetsModule)
     },
     {
         path: '',
